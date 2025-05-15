8214e512ce3bb9c4c4f498e8fcb69b792b3195fa
boneyjoseph29 <161690576+boneyjoseph29@users.noreply.github.com>
2025-05-15 00:32:53 +0000

    let var const

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,4 +1,20 @@
 const accountId = 114556
 let accountEmail = "boney@google.com"
-var accountPasssword ="12345"
+var accountPassword ="12345"
 accountCity = "Kochi"
+let accountState 
+
+// accountId = 2 // Assignment o constant variable not allowed
+accountEmail = "boneyjoseph34@google.com"
+accountPassword = "253689"
+accountCity = "Rome"
+
+/* Prefer not to use var 
+because of block scope and functional scope*/
+
+// console.log(accountId)
+// console.log(accountCity)
+// console.log(accountEmail)
+// console.log(accountPassword)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
