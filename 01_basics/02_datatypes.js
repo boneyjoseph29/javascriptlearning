39da1e8ac7f5290894e20685b4996e178f130feb
boneyjoseph29 <161690576+boneyjoseph29@users.noreply.github.com>
2025-05-16 01:46:05 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) // we are using nodeJS, not browser
+
+
+let name = "boney"
+let age = 33
+let isLoggedin = false
+let state; //undefined
+// number => upto 2^53
+// big int
+// string => ""
+// boolean => true/false
+// null => standalone value
+// symbol => unique
+
+
+//object
+
+// console.log(typeof age)
+//console.log(typeof null); null is an object
+// console.log (typeof undefined); type is undefined
